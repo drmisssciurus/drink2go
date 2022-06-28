70d6f64a82bbca4524b09be1bf2cf2a03f65a9c8
Alina <ms.sciurus@gmail.com>
2022-06-28 10:53:00 +0300

    12

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,37 @@
-// Тут будет код js÷
+// console.log('test');
+
+
+// L.map('map')
+//   .on('load', () => {
+//     console.log('Карта инициализирована');
+//   })
+//   .setView({
+//     lat: 59.92749,
+//     lng: 30.31127,
+//   }, 10);
+
+//   console.log(map);
+
+//   L.tileLayer(
+//     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//     {
+//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     },
+//   ).addTo(map);
+
+// const sliderElement = document.querySelector('.ad-form__slider');
+// const priceElement = document.querySelector('#price');
+
+// noUiSlider.create(sliderElement, {
+//   range: {
+//     min: 0,
+//     max: 1000,
+//   },
+//   start: [0, 900],
+//   step: 1,
+//   connect: true,
+// });
+
+// sliderElement.noUiSlider.on('update', () => {
+//   priceElement.value = sliderElement.noUiSlider.get();
+// });
